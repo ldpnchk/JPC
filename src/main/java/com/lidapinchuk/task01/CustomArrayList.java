be517109c21108935410f155966479c4e327ffef
ldpnchk <lida.pinchuk.26@gmail.com>
2020-11-15 21:19:10 +0200

    Add custom implementation of 'List' interface

---
+++ src/main/java/com/lidapinchuk/task01/CustomArrayList.java
@@ -0,0 +1,454 @@
+package com.lidapinchuk.task01;
+
+import java.util.*;
+
+import static java.lang.String.format;
+import static java.util.Arrays.copyOf;
+import static java.util.Arrays.copyOfRange;
+
+public class CustomArrayList<T> implements List<T> {
+
+    private Object[] elements;
+    private int actualSize;
+
+    public CustomArrayList() {
+        elements = new Object[10];
+    }
+
+    public CustomArrayList(T[] elements) {
+        this.elements = elements;
+        actualSize = elements.length;
+    }
+
+    public int size() {
+        return actualSize;
+    }
+
+    public boolean isEmpty() {
+        return actualSize == 0;
+    }
+
+    public void clear() {
+        elements = new Object[10];
+        actualSize = 0;
+    }
+
+    public boolean contains(Object o) {
+
+        return containsElement(o);
+    }
+
+    private boolean containsElement(Object o) {
+
+        if (o == null) {
+            for (int i = 0; i < actualSize; i++) {
+                if (elements[i] == null) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < actualSize; i++) {
+                if (o.equals(elements[i])) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+
+        for (Object o : c) {
+            if (!containsElement(o)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public T get(int index) {
+
+        checkBoundsExclusively(index);
+
+        return (T) elements[index];
+    }
+
+    private void checkBoundsExclusively(int index) {
+
+        if (index < 0 || index >= actualSize) {
+            throw new IndexOutOfBoundsException(format("Index %d is out of list bounds", index));
+        }
+    }
+
+    private void checkBoundsInclusively(int index) {
+
+        if (index < 0 || index > actualSize) {
+            throw new IndexOutOfBoundsException(format("Index %d is out of list bounds", index));
+        }
+    }
+
+    public int indexOf(Object o) {
+
+        return getIndexOfElement(o);
+    }
+
+    private int getIndexOfElement(Object o) {
+
+        if (o == null) {
+            for (int i = 0; i < actualSize; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < actualSize; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+
+        if (o == null) {
+            for (int i = actualSize - 1; i >= 0; i--) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = actualSize - 1; i >= 0; i--) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean add(T t) {
+
+        increaseSizeIfNecessary(1);
+
+        elements[actualSize] = t;
+
+        actualSize++;
+        return true;
+    }
+
+    private void increaseSizeIfNecessary(int number) {
+
+        int newActualSize = actualSize + number;
+
+        if (newActualSize > elements.length) {
+            int newSize = elements.length;
+            while (newSize < newActualSize) {
+                newSize *= 2;
+            }
+
+            elements = copyOf(elements, newSize);
+        }
+    }
+
+    public void add(int index, T element) {
+
+        checkBoundsInclusively(index);
+
+        increaseSizeIfNecessary(1);
+
+        moveElementsForward(index, 1);
+
+        elements[index] = element;
+        actualSize++;
+    }
+
+    private void moveElementsForward(int startIndex, int number) {
+
+        for (int i = actualSize - 1; i >= startIndex; i--) {
+            elements[i + number] = elements[i];
+        }
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        increaseSizeIfNecessary(c.size());
+
+        for (Object o : c) {
+            elements[actualSize] = o;
+            actualSize++;
+        }
+
+        return true;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> c) {
+
+        checkBoundsInclusively(index);
+
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        increaseSizeIfNecessary(c.size());
+
+        moveElementsForward(index, c.size());
+
+        int currentIndex = index;
+        for (Object o : c) {
+            elements[currentIndex] = o;
+            currentIndex++;
+        }
+
+        actualSize += c.size();
+        return true;
+    }
+
+    public T set(int index, T element) {
+
+        checkBoundsExclusively(index);
+
+        Object oldElement = elements[index];
+        elements[index] = element;
+
+        return (T) oldElement;
+    }
+
+    public boolean remove(Object o) {
+
+        int index = getIndexOfElement(o);
+        if (index == -1) {
+            return false;
+        }
+
+        moveBackward(index, 1);
+        trimTail(actualSize - 1);
+        return true;
+    }
+
+    private void decreaseSizeIfNecessary() {
+
+        int newSize = elements.length / 2;
+        if (actualSize <= newSize) {
+            elements = copyOf(elements, newSize);
+            actualSize = newSize;
+        }
+    }
+
+    public T remove(int index) {
+
+        checkBoundsExclusively(index);
+
+        Object oldElement = elements[index];
+
+        moveBackward(index, 1);
+        trimTail(actualSize - 1);
+        return (T) oldElement;
+    }
+
+    private void moveBackward(int startIndex, int number) {
+        for (int i = startIndex; i < actualSize - 1; i++) {
+            elements[i] = elements[i + number];
+        }
+    }
+
+    public boolean removeAll(Collection<?> c) {
+
+        boolean wasModified = false;
+        int lastIndex = 0;
+        for (int i = 0; i < actualSize; i++) {
+            if (c.contains(elements[i])) {
+                wasModified = true;
+            } else {
+                if (lastIndex != i) {
+                    elements[lastIndex] = elements[i];
+                }
+
+                lastIndex++;
+            }
+        }
+
+        if (!wasModified) {
+            return false;
+        }
+
+        trimTail(lastIndex);
+        return true;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+
+        boolean wasModified = false;
+        int lastIndex = 0;
+        for (int i = 0; i < actualSize; i++) {
+            if (!c.contains(elements[i])) {
+                wasModified = true;
+            } else {
+                if (lastIndex != i) {
+                    elements[lastIndex] = elements[i];
+                }
+
+                lastIndex++;
+            }
+        }
+
+        if (!wasModified) {
+            return false;
+        }
+
+        trimTail(lastIndex);
+        return true;
+    }
+
+    private void trimTail(int lastIndex) {
+
+        for (int i = lastIndex; i < actualSize; i++) {
+            elements[i] = null;
+        }
+        actualSize = lastIndex;
+
+        decreaseSizeIfNecessary();
+    }
+
+    public Object[] toArray() {
+        return copyOf(elements, actualSize);
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+
+        if (a.length < actualSize) {
+            return (T1[]) copyOf(elements, actualSize);
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (i < actualSize) {
+                a[i] = (T1) elements[i];
+            } else {
+                a[i] = null;
+            }
+        }
+        return a;
+    }
+
+    public List<T> subList(int fromIndex, int toIndex) {
+        checkSubListIndexes(fromIndex, toIndex);
+
+        T[] subArray = (T[]) copyOfRange(elements, fromIndex, toIndex);
+        return new CustomArrayList<>(subArray);
+    }
+
+    private void checkSubListIndexes(int fromIndex, int toIndex) {
+        if (fromIndex < 0) {
+            throw new IndexOutOfBoundsException(format("Index %d is out of list bounds", fromIndex));
+        } else if (toIndex > actualSize) {
+            throw new IndexOutOfBoundsException(format("Index %d is out of list bounds", toIndex));
+        } else if (fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException(format("fromIndex %d is bigger than toIndex %d", fromIndex, toIndex));
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new CustomArrayListListIterator();
+    }
+
+    public ListIterator<T> listIterator() {
+        return new CustomArrayListListIterator();
+    }
+
+    private class CustomArrayListListIterator implements ListIterator<T> {
+
+        int currentIndex;
+        int previousIndex = -1;
+
+        CustomArrayListListIterator () {
+
+        }
+
+        CustomArrayListListIterator (int index) {
+            this.currentIndex = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < actualSize - 1;
+        }
+
+        @Override
+        public T next() {
+            if (currentIndex > actualSize - 1) {
+                throw new NoSuchElementException(format("No list element for iterator on position %d", currentIndex));
+            }
+            previousIndex = currentIndex;
+            currentIndex++;
+            return (T) elements[previousIndex];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return currentIndex != 0;
+        }
+
+        @Override
+        public T previous() {
+            if (currentIndex == 0) {
+                throw new NoSuchElementException(format("No list element for iterator on position %d", currentIndex));
+            }
+            previousIndex = currentIndex;
+            currentIndex--;
+            return (T) elements[previousIndex];
+        }
+
+        @Override
+        public int nextIndex() {
+            return currentIndex + 1;
+        }
+
+        @Override
+        public int previousIndex() {
+            return currentIndex - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (previousIndex == -1) {
+                throw new IllegalStateException("'next' or 'previous' operation should be called before that");
+            }
+
+            CustomArrayList.this.remove(previousIndex);
+            currentIndex = previousIndex;
+            previousIndex = -1;
+        }
+
+        @Override
+        public void set(T t) {
+            if (previousIndex == -1) {
+                throw new IllegalStateException("'next' or 'previous' operation should be called before that");
+            }
+
+            CustomArrayList.this.set(previousIndex, t);
+        }
+
+        @Override
+        public void add(T t) {
+
+            int i = currentIndex;
+            CustomArrayList.this.add(i, t);
+            currentIndex = i + 1;
+            previousIndex = -1;
+        }
+    }
+
+    public ListIterator<T> listIterator(int index) {
+        return new CustomArrayListListIterator(index);
+    }
+
+}
---
+++ src/test/java/com/lidapinchuk/task01/CustomArrayListTest.java
@@ -0,0 +1,615 @@
+package com.lidapinchuk.task01;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.EMPTY_LIST;
+import static org.junit.Assert.*;
+
+public class CustomArrayListTest {
+
+    CustomArrayList<String> emptyList;
+    CustomArrayList<String> filledList;
+
+    @Before
+    public void initLists() {
+        emptyList = new CustomArrayList<>();
+        filledList = new CustomArrayList<>(new String[]{
+                "James", "Anna", null, "Tom", "Anna", "Emma", null, "Anna", "Victor", "Tom"
+        });
+    }
+
+    @Test
+    public void testSize_1() {
+
+        int actualResult = emptyList.size();
+
+        assertEquals(0, actualResult);
+    }
+
+    @Test
+    public void testSize_2() {
+
+        int actualResult = filledList.size();
+
+        assertEquals(10, actualResult);
+    }
+
+    @Test
+    public void testIsEmpty_1() {
+
+        boolean actualResult = emptyList.isEmpty();
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void testIsEmpty_2() {
+
+        boolean actualResult = filledList.isEmpty();
+
+        assertFalse(actualResult);
+    }
+
+    @Test
+    public void testClear() {
+
+        filledList.clear();
+
+        assertTrue(filledList.isEmpty());
+        assertEquals(0, filledList.size());
+    }
+
+    @Test
+    public void testContains_1() {
+
+        boolean actualResult = filledList.contains("Anna");
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void testContains_2() {
+
+        boolean actualResult = filledList.contains("Jane");
+
+        assertFalse(actualResult);
+    }
+
+    @Test
+    public void testContains_3() {
+
+        boolean actualResult = filledList.contains(null);
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void testContainsAll_1() {
+
+        boolean actualResult = filledList.containsAll(asList("Anna", "Emma", null));
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void testContainsAll_2() {
+
+        boolean actualResult = filledList.containsAll(asList("Anna", "Jane"));
+
+        assertFalse(actualResult);
+    }
+
+    @Test
+    public void testGet_1() {
+
+        String actualResult = filledList.get(3);
+
+        assertEquals("Tom", actualResult);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGet_2() {
+
+        filledList.get(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGet_3() {
+
+        filledList.get(10);
+    }
+
+    @Test
+    public void testIndexOf_1() {
+
+        int actualResult = filledList.indexOf("Tom");
+
+        assertEquals(3, actualResult);
+    }
+
+    @Test
+    public void testIndexOf_2() {
+
+        int actualResult = filledList.indexOf("Jane");
+
+        assertEquals(-1, actualResult);
+    }
+
+    @Test
+    public void testIndexOf_3() {
+
+        int actualResult = filledList.indexOf(null);
+
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    public void testLastIndexOf_1() {
+
+        int actualResult = filledList.lastIndexOf("Tom");
+
+        assertEquals(9, actualResult);
+    }
+
+    @Test
+    public void testLastIndexOf_2() {
+
+        int actualResult = filledList.lastIndexOf("Jane");
+
+        assertEquals(-1, actualResult);
+    }
+
+    @Test
+    public void testLastIndexOf_3() {
+
+        int actualResult = filledList.lastIndexOf(null);
+
+        assertEquals(6, actualResult);
+    }
+
+    @Test
+    public void testAdd_1() {
+
+        boolean actualResult = filledList.add("Jane");
+
+        assertTrue(actualResult);
+        assertEquals(11, filledList.size());
+        assertEquals("Jane", filledList.get(10));
+    }
+
+    @Test
+    public void testAddIndex_1() {
+
+        filledList.add(2, "Jane");
+
+        assertEquals(11, filledList.size());
+        assertEquals("Jane", filledList.get(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddIndex_2() {
+
+        filledList.add(11, "Jane");
+        assertEquals(10, filledList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddIndex_3() {
+
+        filledList.add(-1, "Jane");
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testAddAll_1() {
+
+        boolean actualResult = filledList.addAll(asList("Jane", null));
+
+        assertTrue(actualResult);
+        assertEquals(12, filledList.size());
+        assertEquals("Jane", filledList.get(10));
+        assertEquals(null, filledList.get(11));
+    }
+
+    @Test
+    public void testAddAll_2() {
+
+        boolean actualResult = filledList.addAll(EMPTY_LIST);
+
+        assertFalse(actualResult);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testAddAllIndex_1() {
+
+        boolean actualResult = filledList.addAll(9, asList("Jane", null));
+
+        assertTrue(actualResult);
+        assertEquals(12, filledList.size());
+        assertEquals("Jane", filledList.get(9));
+        assertEquals(null, filledList.get(10));
+    }
+
+    @Test
+    public void testAddAllIndex_2() {
+
+        boolean actualResult = filledList.addAll(EMPTY_LIST);
+
+        assertFalse(actualResult);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testSet_1() {
+
+        String actualResult = filledList.set(9, "Jane");
+
+        assertEquals("Tom", actualResult);
+        assertEquals(10, filledList.size());
+        assertEquals("Jane", filledList.get(9));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSet_2() {
+
+        String actualResult = filledList.set(9, "Jane");
+
+        filledList.set(10, "Jane");
+        assertEquals(10, filledList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSet_3() {
+
+        String actualResult = filledList.set(9, "Jane");
+
+        filledList.set(-1, "Jane");
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testRemove_1() {
+
+        boolean actualResult = filledList.remove("Tom");
+
+        assertTrue(actualResult);
+        assertEquals(9, filledList.size());
+        assertEquals("Anna", filledList.get(3));
+    }
+
+    @Test
+    public void testRemove_2() {
+
+        boolean actualResult = filledList.remove("Jane");
+
+        assertFalse(actualResult);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testRemove_3() {
+
+        boolean actualResult = filledList.remove(null);
+
+        assertTrue(actualResult);
+        assertEquals(9, filledList.size());
+        assertEquals("Tom", filledList.get(2));
+    }
+
+    @Test
+    public void testRemoveByIndex_1() {
+
+        String actualResult = filledList.remove(9);
+
+        assertEquals("Tom", actualResult);
+        assertEquals(9, filledList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveByIndex_2() {
+
+        filledList.remove(10);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveByIndex_3() {
+
+        filledList.remove(-1);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testRemoveAll_1() {
+
+        boolean actualResult = filledList.removeAll(asList("Tom", null, "Victor"));
+
+        assertTrue(actualResult);
+        assertEquals(5, filledList.size());
+        assertFalse(filledList.contains("Tom") || filledList.contains(null) || filledList.contains("Victor"));
+    }
+
+    @Test
+    public void testRemoveAll_2() {
+
+        boolean actualResult = filledList.removeAll(asList("Jane", "Jack"));
+
+        assertFalse(actualResult);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testRetainAll_1() {
+
+        boolean actualResult = filledList.retainAll(asList("Tom", "Anna", null));
+
+        assertTrue(actualResult);
+        assertEquals(7, filledList.size());
+        assertFalse(filledList.contains("James") || filledList.contains("Emma") || filledList.contains("Victor"));
+    }
+
+    @Test
+    public void testRetainAll_2() {
+
+        boolean actualResult = filledList.retainAll(asList("Tom", "Anna", null, "James", "Emma", "Victor"));
+
+        assertFalse(actualResult);
+        assertEquals(10, filledList.size());
+    }
+
+    @Test
+    public void testToArray_1() {
+
+        Object[] actualResultArray = emptyList.toArray();
+
+        assertArrayEquals(new Object[]{}, actualResultArray);
+    }
+
+    @Test
+    public void testToArray_2() {
+
+        Object[] actualResultArray = filledList.toArray();
+
+        assertArrayEquals(new Object[]{
+                "James", "Anna", null, "Tom", "Anna", "Emma", null, "Anna", "Victor", "Tom"
+        }, actualResultArray);
+    }
+
+    @Test
+    public void testToArrayExisting_1() {
+
+        Object[] actualResultArray = emptyList.toArray(new Object[]{});
+
+        assertArrayEquals(new Object[]{}, actualResultArray);
+    }
+
+    @Test
+    public void testToArrayExisting_2() {
+
+        Object[] actualResultArray = emptyList.toArray(new Object[]{1, 2, 3});
+
+        assertArrayEquals(new Object[]{null, null, null}, actualResultArray);
+    }
+
+    @Test
+    public void testToArrayExisting_3() {
+
+        Object[] actualResultArray = filledList.toArray(new Object[]{
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
+        });
+
+        assertArrayEquals(new Object[]{
+                "James", "Anna", null, "Tom", "Anna", "Emma", null, "Anna", "Victor", "Tom", null, null
+        }, actualResultArray);
+    }
+
+    @Test
+    public void testToArrayExisting_4() {
+
+        Object[] actualResultArray = filledList.toArray(new Object[]{"a", "b", "c"});
+
+        assertArrayEquals(new Object[]{
+                "James", "Anna", null, "Tom", "Anna", "Emma", null, "Anna", "Victor", "Tom"
+        }, actualResultArray);
+    }
+
+    @Test
+    public void testSubList_1() {
+
+        List<String> actualResult = filledList.subList(2, 5);
+
+        assertEquals(3, actualResult.size());
+        assertEquals(null, actualResult.get(0));
+        assertEquals("Tom", actualResult.get(1));
+        assertEquals("Anna", actualResult.get(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSubList_2() {
+
+        filledList.subList(2, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSubList_3() {
+
+        filledList.subList(-1, 2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSubList_4() {
+
+        filledList.subList(2, 11);
+    }
+
+    @Test
+    public void testIteratorHasNext_1() {
+
+        Iterator<String> listIterator = filledList.iterator();
+
+        assertTrue(listIterator.hasNext());
+    }
+
+    @Test
+    public void testIteratorHasNext_2() {
+
+        Iterator<String> listIterator = filledList.iterator();
+
+        for (int i = 0; i < 10; i++) {
+            listIterator.next();
+        }
+
+        assertFalse(listIterator.hasNext());
+    }
+
+    @Test
+    public void testIteratorHasNext_3() {
+
+        Iterator<String> listIterator = emptyList.iterator();
+
+        assertFalse(listIterator.hasNext());
+    }
+
+    @Test
+    public void testIteratorNext_1() {
+
+        Iterator<String> listIterator = filledList.iterator();
+
+        assertEquals("James", listIterator.next());
+        assertEquals("Anna", listIterator.next());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorNext_2() {
+
+        Iterator<String> listIterator = filledList.iterator();
+
+        for (int i = 0; i < 10; i++) {
+            listIterator.next();
+        }
+
+        listIterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorNext_3() {
+
+        Iterator<String> listIterator = emptyList.iterator();
+
+        listIterator.next();
+    }
+
+    @Test
+    public void testIteratorHasPrevious_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        assertFalse(listIterator.hasPrevious());
+    }
+
+    @Test
+    public void testIteratorHasPrevious_2() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.next();
+
+        assertTrue(listIterator.hasPrevious());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorPrevious_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.previous();
+    }
+
+    @Test
+    public void testIteratorPrevious_2() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.next();
+
+        assertEquals("Anna", listIterator.previous());
+    }
+
+    @Test
+    public void testIteratorNextIndex_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        assertEquals(1, listIterator.nextIndex());
+    }
+
+    @Test
+    public void testIteratorPreviousIndex_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        assertEquals(-1, listIterator.previousIndex());
+    }
+
+    @Test
+    public void testIteratorAdd_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.add("James");
+        assertEquals(11, filledList.size());
+        assertEquals("James", filledList.get(0));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIteratorSet_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.set("Jane");
+    }
+
+    @Test
+    public void testIteratorSet_2() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.next();
+        listIterator.set("Jane");
+        assertEquals(10, filledList.size());
+        assertEquals("Jane", filledList.get(0));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIteratorRemove_1() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.remove();
+    }
+
+    @Test
+    public void testIteratorRemove_2() {
+
+        ListIterator<String> listIterator = filledList.listIterator();
+
+        listIterator.next();
+        listIterator.remove();
+        assertEquals(9, filledList.size());
+        assertEquals("Anna", filledList.get(0));
+    }
+
+    @Test
+    public void testIteratorByIndex_1() {
+
+        Iterator<String> listIterator = filledList.listIterator(9);
+
+        assertFalse(listIterator.hasNext());
+    }
+
+}
